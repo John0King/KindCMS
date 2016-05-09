94d5c019e424d601b95b955db28eb6217462c89b
ABlue <ABlue@YIBO>
2016-05-09 17:33:17 +0800

    neight build

---
+++ test/ControllerTest/LegoPageTest/PageListTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllerTest.LegoPageTest
+{
+    public class PageListTest
+    {
+        public void ToPageListTest()
+        {
+
+        }
+    }
+}
